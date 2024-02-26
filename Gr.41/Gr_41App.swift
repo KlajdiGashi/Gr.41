ce95d0d8888d90acf28da035243ebdbfe647a7c6
KlajdiGashi <klajdi.gashi@student.uni-pr.edu>
2024-02-26 13:44:53 +0100

    Initial Commit

---
+++ Gr.41/Gr_41App.swift
@@ -0,0 +1,17 @@
+//
+//  Gr_41App.swift
+//  Gr.41
+//
+//  Created by Gjon Hajdari on 26/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Gr_41App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
